3dd8677d43986e783a45af6fd8a899ceea89f43b
Dakshita Pal <dakshitapal@Dakshitas-MacBook-Air.local>
2024-04-20 14:44:12 -0400

    housing data extacted

--- climate_api.py
+++ climate_api.py
@@ -1,34 +1,4 @@
-"""
-import openmeteo_requests
-from openmeteo_sdk.Variable import Variable 
-
-om = openmeteo_requests.Client()
-params = {
-    "latitude": 52.54,
-    "longitude": 13.41,
-    "hourly": ["temperature_2m", "precipitation"],
-    "current": ["temperature_2m"]
-}
-
-responses = om.weather_api("https://api.open-meteo.com/v1/forecast", params=params)
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-
-# Current values
-current = response.Current()
-current_variables = list(map(lambda i: current.Variables(i), range(0, current.VariablesLength())))
-current_temperature_2m = next(filter(lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2, current_variables))
-# current_relative_humidity_2m = next(filter(lambda x: x.Variable() == Variable.relative_humidity and x.Altitude() == 2, current_variables))  
-
-print(f"Current time {current.Time()}")
-print(f"Current temperature_2m {current_temperature_2m.Value()}")
-# print(f"Current relative_humidity_2m {current_relative_humidity_2m.Value()}") """
- 
-
-
+'''
 import openmeteo_requests
 
 import requests_cache
@@ -46,7 +16,7 @@ url = "https://climate-api.open-meteo.com/v1/climate"
 params = {
 	"latitude": 52.52,
 	"longitude": 13.41,
-	"start_date": "1950-01-01",
+	"start_date": "2025-01-01",
 	"end_date": "2050-12-31",
 	"models": ["CMCC_CM2_VHR4", "FGOALS_f3_H", "HiRAM_SIT_HR", "MRI_AGCM3_2_S", "EC_Earth3P_HR", "MPI_ESM1_2_XR", "NICAM16_8S"],
 	"daily": "temperature_2m_max"
@@ -73,4 +43,49 @@ daily_data = {"date": pd.date_range(
 daily_data["temperature_2m_max"] = daily_temperature_2m_max
 
 daily_dataframe = pd.DataFrame(data = daily_data)
-print(daily_dataframe) 
+print(daily_dataframe) '''
+
+
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://flood-api.open-meteo.com/v1/flood"
+params = {
+	"latitude": 59.91,
+	"longitude": 10.75,
+	"daily": "river_discharge",
+	"forecast_days": 183
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_river_discharge = daily.Variables(0).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["river_discharge"] = daily_river_discharge
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+print(daily_dataframe)
